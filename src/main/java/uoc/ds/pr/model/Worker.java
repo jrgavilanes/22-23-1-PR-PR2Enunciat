ad4220a1a6323833eefd5424e1293958a2c1c1d1
Juan R. Gavilanes <jrgavilanes@gmail.com>
2023-01-07 08:59:28 +0100

    first approach

---
+++ src/main/java/uoc/ds/pr/model/Worker.java
@@ -0,0 +1,60 @@
+package uoc.ds.pr.model;
+
+
+import java.time.LocalDate;
+
+public class Worker {
+    private String dni;
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+    private String roleId;
+
+    public Worker(String dni, String name, String surname, LocalDate birthday, String roleId) {
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.roleId = roleId;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+}
